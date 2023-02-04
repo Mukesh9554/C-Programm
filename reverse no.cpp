97a49dedcab1c2c4eb49d35531b288a8ea9e284e
Mukesh9554 <123925990+Mukesh9554@users.noreply.github.com>
2023-02-03 21:13:19 -0800

    Add files via upload

---
+++ reverse no.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{ int ");
+do
+{
+a= i%10;
+b=i/10;
+i=b;i,a=0,b=0;
+printf("enter the number");
+scanf("%d",i);
+printf("the reverse number
+printf("%d",a);
+} while(i>0);
+
+}
