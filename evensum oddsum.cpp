d1b76fb051ff44adbf2f42a19fd0fe1bce3490ee
Mukesh9554 <123925990+Mukesh9554@users.noreply.github.com>
2023-02-03 21:46:47 -0800

    Update evensum oddsum.cpp

--- evensum oddsum.cpp
+++ evensum oddsum.cpp
@@ -1,20 +1,26 @@
-# include<stdio.h>
-int main()
-{ int i,n,evensum=0,odd=0;
-printf("enter the number:");
-scanf("%d",&n);
-for(i=1;i<=n;i++)
+#include <stdio.h>
+   int main()
 {
-	if(i%2==0)
+	int i,n,f,Oddsum=0,Evensum=0;
+	printf("Enter the Elements:");
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<n;i++)
 	{
-		evensum=evensum+i;
-		
+		scanf("%d",&a[i]);
+		f=a[i]%2;
+		if(f==0){ Evensum=Evensum+a[i];}
+		else{Oddsum=Oddsum+a[i];
+		}
 	}
-	else
-	{odd=odd+i;
-	
-	}}
-	printf("%d\n",evensum);
-	printf("%d",odd);
+			for(i=0;i<n;i++)
+		{
+			printf("printing %d\n",a[i]);
+		}
+			printf("Sum of Even Numers=%d\n",Evensum);
+			printf("Sum of Odd Numers=%d",Oddsum);
 return 0;
-}
+	}
+	
+	
+
